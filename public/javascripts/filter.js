ee8e6add03922999a9b210c2f36cddb937fe6021
dnosit <daniel.nehl@gmail.com>
2020-09-13 11:36:11 +1000

    Deleting all rows except header, but not iterating properly

--- public/javascripts/filter.js
+++ public/javascripts/filter.js
@@ -3,7 +3,7 @@
 const fetchFilteredEvents = (event) => {
     const filter = event.target.textContent;
   
-    removeExistingElements();
+    // removeExistingElements();
 
     fetch(`/user-api/filter/${filter}`)
       .then((res) => res.json())
@@ -20,15 +20,63 @@ const fetchFilteredEvents = (event) => {
       .catch((error) => console.log(error));
 };
 
+
+
+function returnRowList(){
+    return ["147278572", "147782485", "147454127"];
+}
+
+
+
+
+
+// checks and removes elements not longer required on page
+function removeRowsNotRequired(idListOfRows){
+
+    // existing table including header row
+    let tableOfEvents =document.getElementById("tableOfEvents"); 
+    let tableRows = tableOfEvents.rows; 
+    let tableRowCount = tableRows.length;
+
+    // 
+    console.log(tableRowCount);
+    console.log(idListOfRows);
+
+    // elements of the DOM
+    for ( let row of tableRows){
+
+        //console.log(currentID); 
+        if ( row.id != "tableHeaderRow"  ) {
+            console.log(currentID);
+            // remove from DOM table
+            tableOfEvents.deleteRow(row.rowIndex);
+            // removeRowById(currentID);
+        }
+        else {
+            // remove from DOM
+            //removeRowById(currentID);
+        }
+
+
+    }
+    
+
+
+};
+
+
+
+
+
 // add event listener for each drop-down filter option 
 const filterSelected = document.getElementsByClassName("selectShow");
 
 for (let filter of filterSelected) {
-    filter.addEventListener("click", removeExistingElements() );
+    filter.addEventListener("click", removeRowsNotRequired(returnRowList()) );
 }
 
 
-function removeRowById(Id){
-    let row = document.getElementById(Id);
-    row.parentNode.removeChild(row);
-};
+
+
+
+
